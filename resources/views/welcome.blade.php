ad4f44d7e172618cfd4445ac437c3cf936059f6d
unknown <andyaglustman@gmail.com>
2019-07-29 12:00:48 -0300

    mas info hecho

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,11 +111,33 @@
     </div>
     <div class="info-producto-container">
       <h3>{{$product->productName}}  ${{$product->price}}</h3>
-      {{-- <p>{{$product->productDescription}}</p> --}}
-      <a href="#"><button type="button" class="btn btn-info">Mas info</button></a>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter{{$product->id}}">
+        Mas Info
+      </button>
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModalCenter{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">{{$product->productName}}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>{{$product->productDescription}}</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              {{-- <button type="button" class="btn btn-primary">Save changes</button> --}}
+            </div>
+          </div>
+        </div>
+      </div>
       <a href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
-    </div>
+  <!-- Button trigger modal -->
   </div>
+</div>
 @endforeach
 
   </div>
