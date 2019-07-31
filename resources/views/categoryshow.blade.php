9c506c42f23db2386e8e9e2cda914e0d5fb5fc10
unknown <andyaglustman@gmail.com>
2019-07-31 17:59:47 -0300

    modals att diet category

--- resources/views/categoryshow.blade.php
+++ resources/views/categoryshow.blade.php
@@ -14,8 +14,41 @@
     </div>
     <div class="info-producto-container">
       <h3>{{$product->productName}}</h3>
-      {{-- <p>{{$product->productDescription}}</p> --}}
-      <a href="#"><button type="button" class="btn btn-info">Mas info</button></a>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter{{$product->id}}">
+        Mas Info
+      </button>
+      <!-- Modal -->
+      <div class="modal fade welcome" id="exampleModalCenter{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+
+            <div class="modal-header welcome">
+              <h5 class="modal-title" id="exampleModalLongTitle">{{$product->productName}}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
+            <div class="modal-body welcome">
+
+              <div class="img-producto-container-welcome">
+              <img class="imgproducto" src="{{Storage::url($product->img1)}}" alt="">
+              </div>
+
+              <div class="descriptionModal">
+              <p class="proddescmodal">{{$product->productDescription}}</p>
+
+                <p class="prodpricemodal">Precio: <strong>{{$product->price}}</strong></p>
+
+                <a id="addtocartmodal" href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
+              </div>
+
+            </div>
+
+
+          </div>
+        </div>
+      </div>
       <a href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
     </div>
   </div>
