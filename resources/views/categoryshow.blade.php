f038dd2ad2d792f2b85d1799296a105c84a81025
unknown <andyaglustman@gmail.com>
2019-08-04 18:53:06 -0300

    att diet cat show

--- resources/views/categoryshow.blade.php
+++ resources/views/categoryshow.blade.php
@@ -6,17 +6,21 @@
 </head>
 <h2 class="categorynametittle">{{$category->categoryName}}</h2>
 
-<div class="productcategory" style="margin: 0 auto">
+<div class="productcategory" style="margin: 0 auto;">
 @foreach ($category->products as $product)
-  <div class="producto" style="margin: 0 auto">
-    <div class="img-producto-container" style="width: 100%; height: 60%;">
-      <img class="imgproducto" src="{{Storage::url($product->img1)}}" alt="">
+  <div class="producto">
+    <div class="img-producto-container"  style="position: relative">
+      <a href="/productshow/{{$product->id}}">
+      <img class="imgproducto" style="position: relative" src="{{Storage::url($product->img1)}}" alt="" ng-image-appear responsive transition-duration="1s" animation="fadeInUp" animation-duration="1s" easing="ease-out">
+      </a>
+      <button type="button" class="btn" style="color:#393D40; position:absolute; top: 10%; right: 10%" data-toggle="modal" data-target="#exampleModalCenter{{$product->id}}">
+        <i class="fa fa-info-circle" style="font-size:36px"></i>
+      </button>
+      <a style="position:absolute; top: 80%; right: 15%" href="/product/addtocart/{{$product->id}}"><button type="button" class="" style="border-radius:50%; background-color:#D69764; width:2.3em; height:2.3em; color:white; border:solid 0px white"><i class="fas fa-shopping-cart"></i></button></a>
     </div>
     <div class="info-producto-container">
-      <h3>{{$product->productName}}</h3>
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter{{$product->id}}">
-        Mas Info
-      </button>
+      <a style="color:black; text-decoration:none" href="/productshow/{{$product->id}}"> <h3>{{$product->productName}}</h3></a>
+      <p style="margin: 0; font-size:1.2em"><strong>${{$product->price}}</strong></p>
       <!-- Modal -->
       <div class="modal fade welcome" id="exampleModalCenter{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -35,12 +39,17 @@
               <img class="imgproducto" src="{{Storage::url($product->img1)}}" alt="">
               </div>
 
+              <div class="">
               <div class="descriptionModal">
               <p class="proddescmodal">{{$product->productDescription}}</p>
 
-                <p class="prodpricemodal">Precio: <strong>{{$product->price}}</strong></p>
+</div>
+<div class="descriptionModal">
 
-                <a id="addtocartmodal" href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
+                <p class="prodpricemodal">Precio: <strong>{{$product->price}}</strong></p>
+              </div>
+
+                <a id="addtocartmodal" href="/product/addtocart/{{$product->id}}"><button type="button" class="" style="border-radius:50%; background-color:#D69764; width:2.3em; height:2.3em; color:white; border:solid 0px white"><i class="fas fa-shopping-cart"></i></button></a>
               </div>
 
             </div>
@@ -49,7 +58,7 @@
           </div>
         </div>
       </div>
-      <a href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
+
     </div>
   </div>
 @endforeach
