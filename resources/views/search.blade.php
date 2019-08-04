a4cb388b938051a1b0afa2b2d4f2815ae4ceaa5b
unknown <andyaglustman@gmail.com>
2019-08-04 16:51:14 -0300

    view search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -27,18 +27,21 @@
 
 
 
-<div class="productcategory">
+<div class="productcategory" ng-app="myApp">
 @forelse ($queryProductos as $product)
   <div class="producto">
-    <div class="img-producto-container">
-      <img class="imgproducto" src="{{Storage::url($product->img1)}}" alt="">
+    <div class="img-producto-container" ng-controller="myCtrl" style="position: relative">
+      <a href="/productshow/{{$product->id}}">
+      <img class="imgproducto" style="position: relative" src="{{Storage::url($product->img1)}}" alt="" ng-image-appear responsive transition-duration="1s" animation="fadeInUp" animation-duration="1s" easing="ease-out">
+      </a>
+      <button type="button" class="btn" style="color:#393D40; position:absolute; top: 10%; right: 10%" data-toggle="modal" data-target="#exampleModalCenter{{$product->id}}">
+        <i class="fa fa-info-circle" style="font-size:36px"></i>
+      </button>
+      <a style="position:absolute; top: 80%; right: 15%" href="/product/addtocart/{{$product->id}}"><button type="button" class="" style="border-radius:50%; background-color:#D69764; width:2.3em; height:2.3em; color:white; border:solid 0px white"><i class="fas fa-shopping-cart"></i></button></a>
     </div>
     <div class="info-producto-container">
       <a style="color:black; text-decoration:none" href="/productshow/{{$product->id}}"> <h3>{{$product->productName}}</h3></a>
-      <p style="margin: 0;"><strong>${{$product->price}}</strong></p>
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter{{$product->id}}">
-        Mas Info
-      </button>
+      <p style="margin: 0; font-size:1.2em"><strong>${{$product->price}}</strong></p>
       <!-- Modal -->
       <div class="modal fade welcome" id="exampleModalCenter{{$product->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -62,7 +65,7 @@
 
                 <p class="prodpricemodal">Precio: <strong>{{$product->price}}</strong></p>
 
-                <a id="addtocartmodal" href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
+                <a id="addtocartmodal" href="/product/addtocart/{{$product->id}}"><button type="button" class="" style="border-radius:50%; background-color:#D69764; width:2.3em; height:2.3em; color:white; border:solid 0px white"><i class="fas fa-shopping-cart"></i></button></a>
               </div>
 
             </div>
@@ -71,11 +74,11 @@
           </div>
         </div>
       </div>
-      <a href="/product/addtocart/{{$product->id}}"><button type="button" class="btn btn-success"><i class="fas fa-shopping-cart navbutton" id="productCart"></i></button></a>
+
     </div>
   </div>
 @empty
-  <p>Sin resultados en Productos</p>
+  <p>No se encontraron productos en tu búsqueda</p>
 @endforelse
 </div>
 
